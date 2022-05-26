ada8416e59ed93836837131eaff25274192d19e0
eddgeag <eddgeag@gmail.com>
2022-05-26 15:48:43 +0000

    esperando las bases de datos

--- flujo_exomas.R
+++ flujo_exomas.R
@@ -1,5 +1,6 @@
 
 
+
 ## Flujo bioinfomartico para exomas enfermedades geneticas (1)
 ## Basado en subprocesos en R
 
@@ -23,7 +24,7 @@ pipeline <- "../pipeline"
 muestra <- "muestraToy"
 folder_fq <- "fastq_files"
 folder_fasta <- "../datos/cromosoma8"
-
+dir_snpeff <- "~/tools/exomas_tools/snpEff/"
 files_folder <- file.path(pipeline, muestra, folder_fq)
 output_directory <- file.path(pipeline, muestra, "output_dir")
 
@@ -529,81 +530,214 @@ RmDup <- function(input_directory = output_directory) {
 
 
 
-freebayes<-function(input_directory=output_directory,reference_genome =folder_fasta){
-
-  
-  extension = c(unlist(lapply(list.files(reference_genome, pattern = "fa$"), function(x)
-    file_ext(x))),
-  unlist(lapply(list.files(reference_genome, pattern = "fasta$"), function(x)
-    file_ext(x))))
-  print(extension)
-  if (extension== "fasta" | extension == "fa") {
-    extension <- extension
-  } else{
-    errorCondition("No existe archivo fasta")
-  }
-  fasta_file <-
-    list.files(reference_genome,
-               pattern = paste0(".", extension,"$"),
-               full.names = T)
-  print(fasta_file)
-
-
-  output_folder<- file.path(input_directory,"calling_variants")
-  input_folder_bams <-file.path(input_directory,"mapping_output")
-  output_file.vcf <- file.path(output_folder,"freebayes.vcf")
-  rmdup_file <- list.files(input_folder_bams,pattern="rmdup_sorted.bam$",full.names = T)
-
-
-
-  if(!dir.exists(output_folder)){
-    dir.create(output_folder)
-    comando.index <- paste("samtools index ",rmdup_file)
-
-    print(comando.index)
-    system(comando.index)
-
-    comando <- paste("freebayes -f",fasta_file,rmdup_file,">",output_file.vcf )
-    print("LLAMANDO A LAS VARIANTES")
-    print(comando)
-    system(comando)
-
-
-
-
-
-  }else if(dir.exists(output_folder) && !file.exists(output_file.vcf)){
-
-    comando.index <- paste("samtools index ",rmdup_file)
-
-    print(comando.index)
-    system(comando.index)
-
-    comando <- paste("freebayes -f",fasta_file,rmdup_file,">",output_file.vcf )
-    print("LLAMANDO A LAS VARIANTES")
-    print(comando)
-    system(comando)
-
-
-
-
-  }else if(dir.exists(output_folder) && file.exists(output_file.vcf)){
-
-    message("Ya se han llamado a las variantes")
+freebayes <-
+  function(input_directory = output_directory,
+           reference_genome = folder_fasta) {
+    extension = c(unlist(lapply(list.files(reference_genome, pattern = "fa$"), function(x)
+      file_ext(x))),
+      unlist(lapply(list.files(reference_genome, pattern = "fasta$"), function(x)
+        file_ext(x))))
+    print(extension)
+    if (extension == "fasta" | extension == "fa") {
+      extension <- extension
+    } else{
+      errorCondition("No existe archivo fasta")
     }
+    fasta_file <-
+      list.files(reference_genome,
+                 pattern = paste0(".", extension, "$"),
+                 full.names = T)
+    
+    
+    output_folder <- file.path(input_directory, "calling_variants")
+    input_folder_bams <- file.path(input_directory, "mapping_output")
+    output_file.vcf <- file.path(output_folder, "freebayes.vcf")
+    rmdup_file <-
+      list.files(input_folder_bams,
+                 pattern = "rmdup_sorted.bam$",
+                 full.names = T)
+    
+    
+    
+    if (!dir.exists(output_folder)) {
+      dir.create(output_folder)
+      comando.index <- paste("samtools index ", rmdup_file)
+      
+      print(comando.index)
+      system(comando.index)
+      
+      comando <-
+        paste("freebayes -f",
+              fasta_file,
+              rmdup_file,
+              ">",
+              output_file.vcf)
+      print("LLAMANDO A LAS VARIANTES")
+      print(comando)
+      system(comando)
+      
+      
+      
+      
+      
+    } else if (dir.exists(output_folder) &&
+               !file.exists(output_file.vcf)) {
+      comando.index <- paste("samtools index ", rmdup_file)
+      
+      print(comando.index)
+      system(comando.index)
+      
+      comando <-
+        paste("freebayes -f",
+              fasta_file,
+              rmdup_file,
+              ">",
+              output_file.vcf)
+      print("LLAMANDO A LAS VARIANTES")
+      print(comando)
+      system(comando)
+      
+      
+      
+      
+    } else if (dir.exists(output_folder) &&
+               file.exists(output_file.vcf)) {
+      message("Ya se han llamado a las variantes")
+    }
+    
+    
+    
+    
+  }
 
 
 
 
+bcfnorm <-
+  function(input_directory = output_directory,
+           reference_genome = folder_fasta) {
+    extension = c(unlist(lapply(list.files(reference_genome, pattern = "fa$"), function(x)
+      file_ext(x))),
+      unlist(lapply(list.files(reference_genome, pattern = "fasta$"), function(x)
+        file_ext(x))))
+    print(extension)
+    if (extension == "fasta" | extension == "fa") {
+      extension <- extension
+    } else{
+      errorCondition("No existe archivo fasta")
+    }
+    fasta_file <-
+      list.files(reference_genome,
+                 pattern = paste0(".", extension, "$"),
+                 full.names = T)
+    
+    input_file <-
+      file.path(input_directory, "calling_variants", "freebayes.vcf")
+    output_file <-
+      file.path(input_directory, "calling_variants", "freebayes_norm.vcf")
+    
+    if (!file.exists(output_file)) {
+      command <-
+        paste(
+          "bcftools norm --fasta",
+          fasta_file,
+          " --check-ref -w --multiallelics '-both' --site-win 1000 --output-type v ",
+          input_file,
+          " >",
+          output_file
+        )
+      
+      print(command)
+      system(command)
+    } else{
+      message("Ya se ha normalizado")
+    }
+    
+    
+  }
+bcfnorm()
+
+
+
+snpeff <- function(input_directory = output_directory, dir_snpeff) {
+  output_folder <- file.path(input_directory, "anotacion")
+  
+  file_input <-
+    file.path(input_directory, "calling_variants", "freebayes_norm.vcf")
+  
+  file_output1.hg19 <- file.path(output_folder, "annotated_hg19.vcf")
+  file_output2.hg19 <- file.path(output_folder, "annotated_hg19_pred.vcf")
+  file_output3.hg19 <- file.path(output_folder, "annotated__hg19_clin.vcf")
+  
+  file_output1.hg38 <- file.path(output_folder, "annotated_hg38.vcf")
+  file_output2.hg38 <- file.path(output_folder, "annotated_hg38_pred.vcf")
+  file_output3.hg38 <- file.path(output_folder, "annotated__hg38_clin.vcf")
+  
+  snpeff_program <-
+    list.files(dir_snpeff, "snpEff.jar", full.names = T)
+  snpsift_program <-
+    list.files(dir_snpeff, "SnpSift.jar", full.names = T)
+  campos_data_hg19 <-
+    list.files(file.path(dir_snpeff, "data", "dbNSFP", "hg19"), pattern = "gz$")
+  campos_data_hg38 <-
+    list.files(file.path(dir_snpeff, "data", "dbNSFP", "hg38"), pattern = "gz$")
+  
+  clinvar_hg19 <-
+    list.files(file.path(dir_snpeff, "data", "clinvar", "GRCh37"), "gz$")
+  clinvar_hg38 <-
+    list.files(file.path(dir_snpeff, "data", "clinvar", "GRCh37"), "gz$")
+  
+  comando1_hg19 <-
+    paste("java -Xmx4g -jar ",
+          snpeff_program,
+          hg19,
+          "-v",
+          file_input,
+          ">",
+          file_output1.hg19)
+  comando1_hg38 <-
+    paste("java -Xmx4g -jar ",
+          snpeff_program,
+          hg19,
+          "-v",
+          file_input,
+          ">",
+          file_output1.hg38)
+  comando2_hg19 <-
+    paste(
+      "java -Xmx4g -jar ",
+      snpsift_program,
+      "DbNsfp -db",
+      campos_data_hg19,
+      "-f Uniprot_acc,Interpro_domain,1000Gp3_AC,1000Gp3_AFR_AC,1000Gp3_AFR_AF,1000Gp3_EUR_AC,1000Gp3_EUR_AF,1000Gp3_AMR_AC,1000Gp3_AMR_AF,1000Gp3_EAS_AC,1000Gp3_EAS_AF,1000Gp3_SAS_AC,1000Gp3_SAS_AF,ESP6500_AA_AC,ESP6500_AA_AF,ESP6500_EA_AC,ESP6500_EA_AF,ExAC_AC,ExAC_AF,ExAC_Adj_AC,ExAC_Adj_AF,ExAC_AFR_AC,ExAC_AFR_AF,ExAC_AMR_AC,ExAC_AMR_AF,ExAC_EAS_AC,ExAC_EAS_AF,ExAC_FIN_AC,ExAC_FIN_AF,ExAC_NFE_AC,ExAC_NFE_AF,ExAC_SAS_AC,ExAC_SAS_AF,SIFT_pred,SIFT_score,Polyphen2_HDIV_pred,Polyphen2_HDIV_score,Polyphen2_HVAR_pred,Polyphen2_HVAR_score,MutationTaster_pred,MutationTaster_score,phastCons100way_vertebrate,phastCons100way_vertebrate,phyloP100way_vertebrate_rankscore,phyloP30way_mammalian_rankscore,phyloP17way_primate_rankscore,integrated_fitCons_score -v",
+      file_output1.hg19,
+      ">",
+      file_output2.hg19
+    )
+  comando2_hg38 <-
+    paste(
+      "java -Xmx4g -jar ",
+      snpsift_program,
+      "DbNsfp -db",
+      campos_data_hg38,
+      "-f Uniprot_acc,Interpro_domain,1000Gp3_AC,1000Gp3_AFR_AC,1000Gp3_AFR_AF,1000Gp3_EUR_AC,1000Gp3_EUR_AF,1000Gp3_AMR_AC,1000Gp3_AMR_AF,1000Gp3_EAS_AC,1000Gp3_EAS_AF,1000Gp3_SAS_AC,1000Gp3_SAS_AF,ESP6500_AA_AC,ESP6500_AA_AF,ESP6500_EA_AC,ESP6500_EA_AF,ExAC_AC,ExAC_AF,ExAC_Adj_AC,ExAC_Adj_AF,ExAC_AFR_AC,ExAC_AFR_AF,ExAC_AMR_AC,ExAC_AMR_AF,ExAC_EAS_AC,ExAC_EAS_AF,ExAC_FIN_AC,ExAC_FIN_AF,ExAC_NFE_AC,ExAC_NFE_AF,ExAC_SAS_AC,ExAC_SAS_AF,SIFT_pred,SIFT_score,Polyphen2_HDIV_pred,Polyphen2_HDIV_score,Polyphen2_HVAR_pred,Polyphen2_HVAR_score,MutationTaster_pred,MutationTaster_score,phastCons100way_vertebrate,phastCons100way_vertebrate,phyloP100way_vertebrate_rankscore,phyloP30way_mammalian_rankscore,phyloP17way_primate_rankscore,integrated_fitCons_score -v",
+      file_output1.hg38,
+      ">",
+      file_output2.hg38
+    )
+  
+  comando3_clinvar.hg19 <-
+    paste("java -Xmx4g -jar",
+          snpsift_program,
+          "annotate -db",
+          clinvar_hg19)
+  comando3_clinvar.hg38 <-
+    paste("java -Xmx4g -jar",
+          snpsift_program,
+          "annotate -db",
+          clinvar_hg38)
+  
+  
+  
+  
 }
-
-
-freebayes()
-
-
-
-
-
-
-
-
